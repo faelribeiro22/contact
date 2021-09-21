d196a3b89b58a09e583c9866b47aa8bfa1b0f224
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-09-20 21:32:05 -0300

    fix: add public path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'http://localhost:9003/',
+    publicPath: process.env.PUBLIC_PATH,
   },
   mode: 'development',
   devServer: {
